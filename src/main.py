354982fa9b4ddf128098aef9b3150d0ae8353097
CT2005 <cmk4070@gmail.com>
2025-11-10 20:27:33 -0500

    se añaden los metodos para las tareas

--- src/main.py
+++ src/main.py
@@ -57,3 +57,96 @@ def main():
                 break
             else:
                 print(" Opción inválida")
+        if usuario_actual != None:
+            
+            print(f"Usuario: {usuario_actual[3]} ({usuario_actual[1]})")
+            print("\n1.  Crear nueva tarea")
+            print("2.  Ver mis tareas")
+            print("3.  Editar tarea")
+            print("4.  Eliminar tarea")
+            print("5.  Marcar tarea como completada")
+            print("6.  Marcar tarea como pendiente")
+            """print("7.  Ver recordatorios")"""
+            print("8.  Cerrar sesión")
+            
+            opcion = input("Selecciona una opción: ")
+            
+            if opcion == "1":
+                print("\n--- CREAR NUEVA TAREA ---")
+                titulo = input("Título de la tarea: ")
+                descripcion = input("Descripción: ")
+                
+                prioridad = input("Prioridad (alta/media/baja): ")
+                
+                if crear_tarea(usuario_actual[0], titulo, descripcion, prioridad):
+                    print(" Tarea creada exitosamente")
+                else:
+                    print(" Error al crear la tarea")
+                    
+            elif opcion == "2":
+                tarea = obtener_tarea_usuario(usuario_actual[0])
+                mostrar_tareas(tarea)
+                
+            elif opcion == "3":
+                tarea = obtener_tarea_usuario(usuario_actual[0])
+                if tarea:
+                    mostrar_tareas(tarea)
+                    tarea_id = input("\nID de la tarea a editar: ")
+                    titulo = input("Nuevo título: ")
+                    descripcion = input("Nueva descripción: ")
+                    prioridad = input("Nueva prioridad: ")
+                    
+                    if actualizar_tarea(tarea_id, titulo, descripcion, prioridad):
+                        print(" Tarea actualizada")
+                    else:
+                        print(" Error al actualizar")
+                else:
+                    print(" No hay tareas para editar")
+                    
+            elif opcion == "4":
+                tarea = obtener_tarea_usuario(usuario_actual[0])
+                if tarea:
+                    mostrar_tareas(tarea)
+                    tarea_id = input("\nID de la tarea a eliminar: ")
+                    confirm = input("¿Estás seguro? (s/n): ")
+                    if confirm.lower() == 's':
+                        if borrar_tarea(tarea_id):
+                            print(" Tarea eliminada")
+                        else:
+                            print(" Error al eliminar")
+                else:
+                    print(" No hay tareas para eliminar")
+                    
+            elif opcion == "5":
+                tarea = obtener_tarea_usuario(usuario_actual[0])
+                if tarea:
+                    mostrar_tareas(tarea)
+                    tarea_id = input("\nID de la tarea a marcar como COMPLETADA: ")
+                    if actualizar_estado_tarea(tarea_id, "completada"):
+                        print(" Tarea marcada como completada")
+                    else:
+                        print(" Error al actualizar")
+                else:
+                    print(" No hay tareas")
+                    
+            elif opcion == "6":
+                tarea = obtener_tarea_usuario(usuario_actual[0])
+                if tarea:
+                    mostrar_tareas(tarea)
+                    tarea_id = input("\nID de la tarea a marcar como PENDIENTE: ")
+                    if actualizar_estado_tarea(tarea_id, "pendiente"):
+                        print(" Tarea marcada como pendiente")
+                    else:
+                        print(" Error al actualizar")
+                else:
+                    print(" No hay tareas")
+                    
+                
+            elif opcion == "8":
+                usuario_actual = logout_usuario()
+                
+            else:
+                print(" Opción inválida")
+
+if _name_ == "_main_":
+    main()    
